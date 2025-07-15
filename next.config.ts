84a03c0aa16da7a923fb0a1b92e287a118e2edab
RO <director.operativo@ialatinoamerica.co>
2025-07-14 20:50:18 -0500

    Configure environment variables and update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // Recomendado para Nixpacks o Docker
+  // basePath: '/subruta', // Descomenta y configura si tu app está detrás de una subruta
 };
 
 export default nextConfig;
